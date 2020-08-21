c59587aec60f51546ed385cae1387390cb39e9fb
kristymz <69281576+kristymz@users.noreply.github.com>
2020-08-21 00:45:53 -0400

    Add files via upload
    Added gradient background on mainpage

---
+++ theTracker/theTracker/UIView+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+Extension.swift
+//  theTracker
+//
+//  Created by kristy awesome pants martinez on 8/21/20.
+//  Copyright © 2020 kristy martinez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    func setGradientBackground(colorOne:UIColor, colorTwo: UIColor) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradientLayer.locations = [0.0,1.0]
+        gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y:0.0)
+        
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
---
+++ theTracker/theTracker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  theTracker
+//
+//  Created by kristy awesome pants martinez on 8/19/20.
+//  Copyright © 2020 kristy martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.setGradientBackground(colorOne: Colors.red, colorTwo: Colors.orange)
+    }
+
+
+}
+
