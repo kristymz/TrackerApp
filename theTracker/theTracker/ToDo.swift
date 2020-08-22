51e49c968306680ab69d8c5966d50aacd84f0531
kristymz <69281576+kristymz@users.noreply.github.com>
2020-08-21 20:24:01 -0400

    Add files via upload
    finished part 1 of 3 for list

---
+++ theTracker/theTracker/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  theTracker
+//
+//  Created by kristy awesome pants martinez on 8/21/20.
+//  Copyright © 2020 kristy martinez. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    var description = ""
+    var important = false
+}
